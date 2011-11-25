5cbfd7183f6995af10c36db9fc7213b4b6b86949
mark <mark@zed.(none)>
2011-11-25 22:04:22 +1100

    Initial Commit

---
+++ smc_utils/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  smc_utils
+//
+//  Created by Mark Garrett on 25/11/2011.
+//  Copyright (c) 2011 Garetech Computer Solutions. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
